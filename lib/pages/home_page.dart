5acd8d497b513cbf5a187a3e26f11388012acdfc
Calvin <CalvinB552@gmail.com>
2024-12-29 19:24:18 -0500

    buttons

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,14 +9,58 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+      page = Placeholder();
+      break;
+    case 1:
+      page = Placeholder();
+      break;
+    case 2:
+      page = Placeholder();
+      break;
+    case 3:
+      page = Placeholder();
+      break;
+    default:
+      throw UnimplementedError('no widget for $selectedIndex')
+    }
+
     return Scaffold(
       appBar: AppBar(title: const Text('overwatch stats'),),
-      body: Column(
-        children: [const Text("hello"),],
-      )
-    );
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: () {
+
+            },
+             
+            child: const Text('Heros')),
+          ElevatedButton(onPressed: () {
+
+            },
+             
+            child: const Text('Players')),
+          ElevatedButton(onPressed: () {
+
+            },
+             
+            child: const Text('Maps')),
+          ElevatedButton(onPressed: () {
+
+            },
+             
+            child: const Text('gamemodes')),
+          ],
+        )
+
+        ),
+      );
   }
 }
