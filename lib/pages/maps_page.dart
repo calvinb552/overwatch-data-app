782bdb22d78793d3c5e82c3806d6a47ec663da10
Calvin <CalvinB552@gmail.com>
2025-01-22 14:27:31 -0500

    actuall map template

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -7,6 +7,15 @@ class Maps_Page extends StatefulWidget{
 }
 
 class _Maps_PageState extends State<Maps_Page> {
+  bool _showImage = true;
+  
+
+  void _toggleView() async {
+    await Future.delayed(const Duration(milliseconds: 200)); //wait for ripple affect to start
+    setState(() {
+      _showImage = !_showImage; //switch image on and off
+    });
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -17,31 +26,47 @@ class _Maps_PageState extends State<Maps_Page> {
         centerTitle: true,
         backgroundColor: Color.fromARGB(255, 51, 51, 190),),
       body: Center(
-        child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+        child: Material(
+        color: Colors.blue,
+        elevation: 8,
+        borderRadius: BorderRadius.circular(28),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child:
           InkWell(
             splashColor: Colors.blueAccent,
-            onTap: () {},
+            onTap: () {
+              _toggleView();
+            },
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [ 
-            Ink.image(
+              SizedBox(
+                height: 400,
+                width: 400,
+                child: 
+              _showImage 
+              ?Ink.image(
               image: const NetworkImage('https://overfast-api.tekrop.fr/static/maps/necropolis.jpg'),
-              height: 200,
-              width: 200,
+              height: 400,
+              width: 400,
               fit: BoxFit.cover,
-              ),
-              SizedBox(height: 6),
-              Text('Map Name',
+              )
+              :const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('info goes here',
+                style: TextStyle(fontSize: 16),),)),
+              const SizedBox(height: 6),
+              const Text('Map Name',
               style: TextStyle(fontSize: 32),)
-              
             ]
           )
           )
-        ],
       ),
     )
     );
   }
 }
+
+
+
+
