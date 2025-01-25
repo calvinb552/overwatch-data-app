2b1afb4fac59851b3420d6ea01044c27e6be9cab
Calvin <CalvinB552@gmail.com>
2025-01-24 20:08:16 -0500

    set up gamemodes api call functions because I'm stuck on the search bar

--- lib/pages/gamemode_page.dart
+++ lib/pages/gamemode_page.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
+import 'dart:convert'; // For JSON decoding
+import 'package:http/http.dart' as http;
 
+Future<List<gamemodes>> fetchGamemodes() async{
+  final response = await http.get(Uri.parse('https://overfast-api.tekrop.fr/gamemodes'));
+
+  if(response.statusCode == 200) {
+    List<dynamic> gamemode_list = jsonDecode(response.body);
+    return gamemode_list.map((json) => gamemodes.fromJson(json)).toList();
+  }else{
+    throw Exception('failed to load api');
+  }
+}
 class Gamemode_Page extends StatefulWidget{
   const Gamemode_Page({Key? key}) : super(key:key);
   @override
@@ -23,5 +35,31 @@ class _Gamemode_PageState extends State<Gamemode_Page> {
       ),
     )
     );
+  }
+}
+
+
+class gamemodes {
+  final String gamekey;
+  final String name;
+  final String icon;
+  final String description;
+  final String screenshot;
+
+  const gamemodes({
+    required this.gamekey,
+    required this.name,
+    required this.icon,
+    required this.description,
+    required this.screenshot,
+  });
+
+  factory gamemodes.fromJson(Map<String,dynamic>json){
+    return gamemodes(
+      gamekey: json['key'], 
+      name: json['name'], 
+      icon: json['icon'], 
+      description: json['description'], 
+      screenshot: json['screenshot']);
   }
 }
