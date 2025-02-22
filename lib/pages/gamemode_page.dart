0071339de68d8c2a1cf2136085a23d0754678343
Calvin <CalvinB552@gmail.com>
2025-02-22 16:51:45 -0500

    images still don't work uhgg

--- lib/pages/gamemode_page.dart
+++ lib/pages/gamemode_page.dart
@@ -2,6 +2,21 @@ import 'package:flutter/material.dart';
 import 'dart:convert'; // For JSON decoding
 import 'package:http/http.dart' as http;
 
+Map<String, String> Gamemode_images = {
+    "assault":"https://static.wikia.nocookie.net/overwatch_gamepedia/images/e/e0/Assault.png/revision/latest/scale-to-width-down/256?cb=20230516191316",
+    "capture-the-flag":"",
+    "clash":"",
+    "control":"",
+    "deathmatch":"",
+    "elimination":"",
+    "escort":"",
+    "flashpoint": "",
+    "hybrid":"",
+    "push": "",
+    "team-deathmatch":""
+
+  };
+
 Future<List<gamemodes>> fetchGamemodes() async{
   final response = await http.get(Uri.parse('https://overfast-api.tekrop.fr/gamemodes'));
 
@@ -49,6 +64,8 @@ class _Gamemode_PageState extends State<Gamemode_Page> {
               itemCount: gamemode.length,
               itemBuilder: (context, index) {
                 final item = gamemode[index];
+
+                print('Loading image for ${item.name}: ${item.icon}');
                 return Column(
                   children: [
                     GestureDetector(
@@ -69,13 +86,24 @@ class _Gamemode_PageState extends State<Gamemode_Page> {
                         children: [
                           //icon
                           Container(
-                            padding: EdgeInsets.all(12),
-                            decoration: BoxDecoration(
+                            padding: const EdgeInsets.all(12),
+                            decoration: const BoxDecoration(
                               shape: BoxShape.circle,
                               color: Colors.greenAccent,
                             ),
-                            child: Image.network(
-                              item.icon
+                            child: Container(
+                              color: Colors.red,
+                              width: 60,
+                              height: 60,
+                              child: 
+                            Image.network(
+                              item.icon,
+                              width: 50,
+                              height: 50,
+                              errorBuilder: (context, error, StackTrace){
+                                return Image.network("https://e7.pngegg.com/pngimages/10/205/png-clipart-computer-icons-error-information-error-angle-triangle-thumbnail.png");
+                              },
+                            ),
                             ),
                           ),
                           if (index != gamemode.length - 1)
@@ -102,13 +130,14 @@ class _Gamemode_PageState extends State<Gamemode_Page> {
 
 
 class gamemodes {
+
   final String gamekey;
   final String name;
   final String icon;
   final String description;
   final String screenshot;
 
-  const gamemodes({
+  gamemodes({
     required this.gamekey,
     required this.name,
     required this.icon,
@@ -120,7 +149,7 @@ class gamemodes {
     return gamemodes(
       gamekey: json['key'], 
       name: json['name'], 
-      icon: json['icon'], 
+      icon: Gamemode_images[json["key"]] ?? "https://e7.pngegg.com/pngimages/10/205/png-clipart-computer-icons-error-information-error-angle-triangle-thumbnail.png", 
       description: json['description'], 
       screenshot: json['screenshot']);
   }
